c73184d0f57a28150bc1b2fb0d1033d32b356209
Вячеслав Владимирович <zvv56739@gmail.com>
2021-01-10 15:55:28 -0500

    1. Заново создал спрайт - подключил иконки 2.внес изменения в блок wislist 3.Внес изменения в блок search-box 4. Создал разметку для следующего раздела 5. Внес модификатор в блок categories -> categories__item -> categories__item--opened

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $('.lang-menu').on('click', function(){
     $('.lang-menu').toggleClass('lang-menu--click')
     $('.lang-menu').trigger('close');
+});
+
+$('.categories__item').on('click', function(){
+    $('.categories__item').toggleClass('categories__item--opened')
+    $('categories__item').trigger('close');
 })
