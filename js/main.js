0359f0f25d8c00da357f56d84822ac0cf2aed4ea
Вячеслав Владимирович <zvv56739@gmail.com>
2021-01-14 20:35:47 -0500

    добавил блок PROMO

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ $('.lang-menu').on('click', function(){
 $('.categories__item').on('click', function(){
     $('.categories__item').toggleClass('categories__item--opened')
     $('categories__item').trigger('close');
-})
+});
+
+$('.promo__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+});
